ad38e17df4eeffdf1b052ee98784658cf45eab17
fladeia <252842.5s@gmail.com>
2021-09-16 10:36:47 -0300

    set marker and map location

--- index.js
+++ index.js
@@ -9,24 +9,28 @@ let ip
 let latitude = -25.4612667
 let longitude = -49.28532
 
+// ----------- map 
 const apiUrlMap = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 const attribution = 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors</a>'
+let mymap = L.map('mapid').setView([latitude, longitude], 14);
+let marker = L.marker([latitude, longitude]).addTo(mymap);
+
+L.tileLayer(apiUrlMap, { attribution }).addTo(mymap);
+// ----------- map 
+
 const apiUrlIp = `https://geo.ipify.org/api/v1?apiKey=at_nl22wkvVRmAYq4wTQ6VoJOGx3es1p&ipAddress=${ip}`
 const apiUrlIpLocal = 'https://api.ipify.org?format=json'
-
 
 async function fetchLocalIp() {
     resLocal = await fetch(apiUrlIpLocal)
     dataLocal = await resLocal.json()
     ip = dataLocal.ip
-    console.log(ip);
     fetchData(ip)
 }
 fetchLocalIp()
 
 // AdBlock chrome extension may cause error "net::ERR_BLOCKED_BY_CLIENT" 
 async function fetchData(ip) {
-
     res = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_nl22wkvVRmAYq4wTQ6VoJOGx3es1p&ipAddress=${ip}`)
     data = await res.json()
 
@@ -34,14 +38,14 @@ async function fetchData(ip) {
     locationShow.innerHTML = `${data.location.city}, ${data.location.country} ${data.location.postalCode}`
     utcShow.innerHTML = `UTC${data.location.timezone}`
     ispShow.innerHTML = data.isp
+    latitude = data.location.lat
+    longitude = data.location.lng
+    mymap.setView([latitude, longitude])
+    marker.setLatLng([latitude, longitude])
+
 }
 
 btn.addEventListener('click', () => {
     ip = ipInput.value
     fetchData(ip)
 })
-
-const mymap = L.map('mapid').setView([latitude, longitude], 14);
-const marker = L.marker([latitude, longitude]).addTo(mymap);
-
-L.tileLayer(apiUrlMap, { attribution }).addTo(mymap);
